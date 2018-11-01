40170de9aff9d5a91c1ab47658114892126d379e
Kashif1Naqvi <naqvishah582@gmail.com>
2018-11-01 12:17:33 +0500

    movie titile

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import green from './green.png'
 class App extends Component {
   render() {
     return (
-
-      <div className="App">
-        <h1>hello</h1>  
-      </div>
+      
+    <div className="App">
+         <table className="titleBar" >
+           <tbody>
+             <tr>
+               <td>
+                 <img width="50" src={green} alt="app icon"/>
+               </td>
+               <td width="8" />
+               <td>
+                    <h1>MovieDB Search</h1>
+               </td>
+             </tr>
+           </tbody>
+         </table>
+         <input style={{
+           fontSize:24,
+           display:'block',
+           widthy:"99%",
+           paddingTop:8,
+           paddingLeft:16
+         }}  placeholder="Enter search" />
+    </div>
     );
   }
 }
