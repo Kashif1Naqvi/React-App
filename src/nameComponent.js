45003da472db8dacd6b2c9e60875775c6803a392
Kashif1Naqvi <naqvishah582@gmail.com>
2018-11-01 11:11:30 +0500

    first commit

---
+++ src/nameComponent.js
@@ -0,0 +1,12 @@
+import React,{Component} from "react";
+
+class  NameComponent extends Component{
+    render(){
+        return(
+            <div>
+                hello kashif
+            </div>
+        )
+    }
+}  
+export default NameComponent;
